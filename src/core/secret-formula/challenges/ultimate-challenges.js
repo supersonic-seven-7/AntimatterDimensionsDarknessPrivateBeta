de9ca79b5cd37e5a9d7f685b371e5fa5cd3cd538
Supersonic Seven <b.rockholz@icloud.com>
2025-11-23 11:49:34 -0500

    Update ultimate-challenges.js
    And fix that

--- src/core/secret-formula/challenges/ultimate-challenges.js
+++ src/core/secret-formula/challenges/ultimate-challenges.js
@@ -31,7 +31,7 @@ export const ultimateChallenges = [
     id: 3,
     description: () =>
       `You are trapped in every Eternity Challenge AT THE SAME TIME.`,
-    goal: () => Decimal.max(Decimal.NUMBER_MAX_VALUE, player.records.thisReality.bestAMUC3),
+    goal: Decimal.NUMBER_MAX_VALUE,
     isQuickResettable: true,
     reward: {
       description:

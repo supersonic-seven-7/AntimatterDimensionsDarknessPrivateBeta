7c96f84721114be9d051259ad25b4a3e5b2d7a49
Supersonic Seven <b.rockholz@icloud.com>
2025-12-01 10:03:08 -0500

    Update TimeDilationTab.vue
    Just to place on visuals

--- src/components/tabs/time-dilation/TimeDilationTab.vue
+++ src/components/tabs/time-dilation/TimeDilationTab.vue
@@ -109,7 +109,7 @@ export default {
       this.baseGalaxies = player.dilation.baseTachyonGalaxies;
       this.totalGalaxies = player.dilation.totalTachyonGalaxies;
       this.hasPelleDilationUpgrades = PelleRifts.paradox.milestones[0].canBeApplied;
-      if (this.baseGalaxies < 500 && DilationUpgrade.doubleGalaxies.isBought) {
+      if (this.baseGalaxies < 50 && DilationUpgrade.doubleGalaxies.isBought) {
         this.tachyonGalaxyGain = DilationUpgrade.doubleGalaxies.effectValue;
       } else {
         this.tachyonGalaxyGain = 1;
